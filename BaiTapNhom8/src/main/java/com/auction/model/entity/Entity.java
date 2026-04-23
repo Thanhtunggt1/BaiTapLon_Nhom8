da237a0096e81a0f44b3ec1894cacf2b23c0b148
TranThanhTung <thanhtunggt1@gmail.com>
2026-04-23 16:04:26 +0700

    Tung

--- BaiTapNhom8/src/main/java/com/auction/model/entity/Entity.java
+++ BaiTapNhom8/src/main/java/com/auction/model/entity/Entity.java
@@ -7,17 +7,26 @@ import java.util.UUID;
  * Lớp cơ sở trừu tượng cho mọi đối tượng trong hệ thống.
  * Cung cấp id duy nhất (UUID) và thời điểm tạo.
  */
+
 public abstract class Entity {
 
-    private final String id;
-    private final LocalDateTime createdAt;
+    private final String id;                // Mã định danh
+    private final LocalDateTime createdAt;  // Thời gian chính xác mà một đối tượng được sinh ra
 
     protected Entity() {
         this.id = UUID.randomUUID().toString();
-        this.createdAt = LocalDateTime.now();
+
+        /*
+        UUID.randomUUID() kết quả VD: 5fc03087-d265-41e7-b8c6-83e29cd24f4c
+        * Luôn gồm 36 ký tự theo định dạng 8-4-4-4-12, ký tự chỉ bao gồm các số từ 0-9 và các chữ cái từ a-f
+        * Tỷ lệ sinh ra 2 mã UUID ngẫu nhiên giống hệt nhau là cực kỳ thấp (gần như bằng 0)
+        * .toString ép ra kiểu String
+        * */
+
+        this.createdAt = LocalDateTime.now(); // Gán bằng thời gian thực
     }
 
-    // ── Getters ──────────────────────────────────────────────────────────────
+    //Getters
 
     public String getId() {
         return id;
@@ -27,7 +36,7 @@ public abstract class Entity {
         return createdAt;
     }
 
-    // ── Object overrides ─────────────────────────────────────────────────────
+    //Object overrides
 
     @Override
     public boolean equals(Object o) {
@@ -39,11 +48,19 @@ public abstract class Entity {
 
     @Override
     public int hashCode() {
-        return id.hashCode();
+        return id.hashCode(); // .hasCode chuyển String id -> int id
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "{id='" + id + "'}";
+        /*
+        * getClass() mò ra được đối tợng thuộc kiểu gì thực sự
+        * VD: Item myItem = new Electronics(.....);
+        * myItem.getClass -> Class Electronics
+        * getClass() trả về một kiểu dữ liệu rất đặc biệt trong Java, nó có tên chính là Class (viết hoa chữ C, thuộc gói java.lang)
+        * getSimpleName()
+        * */
+
     }
 }
